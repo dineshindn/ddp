30ea3b8c75a4bff3601b765d9ed2bec3c2cf16f1
dinesh <dinesh.n@klinikeverywhere.com>
2022-06-07 17:23:06 +0530

    Created new module folders

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,21 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CoreModule } from './core_/core.module';
+import { RegisterComponent } from './modules_/register/register.component';
+import { LoginComponent } from './modules_/login/login.component';
+import { ProjectsComponent } from './modules_/projects/projects.component';
+import { TeamsComponent } from './modules_/teams/teams.component';
+import { DashboardComponent } from './modules_/dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    RegisterComponent,
+    LoginComponent,
+    ProjectsComponent,
+    TeamsComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
